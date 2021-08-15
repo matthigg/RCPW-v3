777e8381253164cdec8a7220c9fbdd2c59ce34ef
Matt H <mhh129@gmail.com>
2021-08-15 14:14:02 -0400

    Run npm i, change all instances of rns-v2 to rcpw-v3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'rns-v2'`, () => {
+  it(`should have as title 'rcpw-v3'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('rns-v2');
+    expect(app.title).toEqual('rcpw-v3');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('rns-v2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('rcpw-v3 app is running!');
   });
 });
