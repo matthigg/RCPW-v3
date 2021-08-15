268dea3d97a214a66b8d26210ac6f016bc4ac825
Matt H <mhh129@gmail.com>
2021-08-15 14:30:48 -0400

    Comment out references to RnS aws resources

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  contactFormAPI: 'https://olp4b4q411.execute-api.us-east-1.amazonaws.com/prod/contact-form',
+  // contactFormAPI: 'https://olp4b4q411.execute-api.us-east-1.amazonaws.com/prod/contact-form',
+  contactFormAPI: '',
   production: true,
-  rootImageURL: 'https://rns-static-and-media-files.s3.amazonaws.com/media/images/',
+  // rootImageURL: 'https://rns-static-and-media-files.s3.amazonaws.com/media/images/',
+  rootImageURL: '',
 };
